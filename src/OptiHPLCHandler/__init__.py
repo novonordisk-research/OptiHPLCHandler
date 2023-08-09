3a44bfa8ca560390b0e998a7ee15a9339f6c1064
GitHub Action <build_pipeline@novonordisk.com>
2023-08-09 09:26:59 +0000

    bump version 0.2.5 -> 0.2.6

--- src/OptiHPLCHandler/__init__.py
+++ src/OptiHPLCHandler/__init__.py
@@ -2,6 +2,6 @@ from .data_types import DataField, HPLCSetup, Sample
 from .empower_api_core import EmpowerConnection
 from .empower_handler import EmpowerHandler
 
-__version__ = "0.2.5"
+__version__ = "0.2.6"
 
 __all__ = ["DataField", "EmpowerConnection", "EmpowerHandler", "HPLCSetup", "Sample"]
