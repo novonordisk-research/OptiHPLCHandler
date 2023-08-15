6ca7059ac20e9cd79a883060b274febfec46568b
SRFU <srfu@novonordisk.com>
2023-08-15 09:05:05 +0200

    feat: Get node names

--- tests/test_proxy_api.py
+++ tests/test_proxy_api.py
@@ -363,3 +363,12 @@ class TestEmpowerHandler(unittest.TestCase):
         mock_requests.get.return_value = mock_response
         self.handler.GetPlateTypeNames(filter_string="test_filter_text")
         assert "?stringFilter=test_filter_text" in mock_requests.get.call_args[0][0]
+
+    @patch("OptiHPLCHandler.empower_api_core.requests")
+    def test_get_node_name_list(self, mock_requests):
+        mock_response = MagicMock()
+        mock_response.json.return_value = {"results": ["test_node_name_1"]}
+        mock_requests.get.return_value = mock_response
+        node_name_list = self.handler.GetNodeNameList()
+        assert node_name_list == ["test_node_name_1"]
+        assert "acquisition/nodes" in mock_requests.get.call_args[0][0]
