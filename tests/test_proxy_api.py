a9bc433f3ec06a4b96042a95ecd0e4fa2c488eed
SRFU <srfu@novonordisk.com>
2023-09-11 09:25:45 +0200

    test: Testing setters and giving username

--- tests/test_proxy_api.py
+++ tests/test_proxy_api.py
@@ -364,3 +364,32 @@ class TestEmpowerHandler(unittest.TestCase):
         with self.handler:
             pass
         assert self.handler.connection.login.call_count == 0
+
+    def test_project_setter(self):
+        self.handler.project = "test_project"
+        with self.handler:
+            pass
+        assert self.handler.connection.project == "test_project"
+
+    def test_address_setter(self):
+        self.handler.address = "test_address"
+        with self.handler:
+            pass
+        assert self.handler.connection.address == "test_address"
+
+    def test_username_setter(self):
+        self.handler.username = "test_username"
+        with self.handler:
+            pass
+        assert self.handler.connection.username == "test_username"
+
+    @patch("OptiHPLCHandler.empower_handler.EmpowerConnection")
+    def test_username_on_creation(self, mock_connection):
+        mock_response = MagicMock
+        mock_connection.return_value = mock_response
+        EmpowerHandler(
+            project="test_project",
+            address="https://test_address/",
+            username="test_username",
+        )
+        assert mock_response.username == "test_username"
