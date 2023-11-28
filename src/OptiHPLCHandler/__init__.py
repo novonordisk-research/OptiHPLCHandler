928deda570d6287c9be8527a694bb471f867c311
GitHub Action <build_pipeline@novonordisk.com>
2023-11-28 12:08:39 +0000

    bump version 2.4.0 -> 2.4.1

--- src/OptiHPLCHandler/__init__.py
+++ src/OptiHPLCHandler/__init__.py
@@ -4,7 +4,7 @@ from .empower_handler import EmpowerHandler
 from .empower_instrument_method import EmpowerInstrumentMethod
 from .empower_module_method import EmpowerModuleMethod
 
-__version__ = "2.4.0"
+__version__ = "2.4.1"
 
 __all__ = [
     "DataField",
