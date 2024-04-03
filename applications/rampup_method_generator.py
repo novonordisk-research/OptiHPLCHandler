0fc62971ed997a909957fa70ccf92ca3461606fa
SLCB-NN <slcb@novonordisk.com>
2024-04-03 14:58:06 +0200

    feat: trim py file

--- applications/rampup_method_generator.py
+++ applications/rampup_method_generator.py
@@ -1,11 +1,44 @@
 from OptiHPLCHandler import EmpowerHandler
+from typing import List
 
-# Create an instance of the EmpowerHandler class
-handler = EmpowerHandler(project="WebAPI_test", address="XXXX")
 
-# Get the list of methods, select one, and get the method details
-with handler:
-    method_list = handler.GetMethodList()  # Get the list of instrument methods
-    method_name = method_list[0]  # Select the first method
-    print(method_name)
-    full_method = handler.GetInstrumentMethod(method_name)
+def generate_rampup_method(
+    full_method: EmpowerHandler,
+    rampup_time: int = 10,
+    low_flow_rate: float = 0.05,
+    flow_curve: int = 6,
+) -> EmpowerHandler:
+    """
+    Generate a ramp-up method based on the given full method.
+
+    Args:
+        full_method (EmpowerHandler): The full method to generate the ramp-up method from.
+        rampup_time (int): The ramp-up time in seconds. Default is 10.
+        low_flow_rate (float): The low flow rate in mL/min. Default is 0.05.
+        flow_curve (int): The flow curve number. Default is 6.
+
+    Returns:
+        EmpowerHandler: The generated ramp-up method.
+    """
+    # Get the gradient table and method name from the full method
+    gradient_table: List[dict] = full_method.gradient_table
+    method_name: str = full_method.method_name
+
+    # Truncate the method name if it is too long
+    if len(method_name) > 25:
+        method_name = method_name[:25]
+
+    method_name = f"{method_name}_ramp"
+
+    # Generate a new gradient table for the ramp-up method
+    gradient_table = [
+        gradient_table[0],
+        {**gradient_table[0], "Time": rampup_time, "Curve": flow_curve},
+    ]
+    gradient_table[0]["Flow"] = low_flow_rate
+
+    # Set method name and gradient table in the full method
+    full_method.gradient_table = gradient_table
+    full_method.method_name = method_name
+
+    return full_method
