0d1af0226ac985b35c74f6a89da53ac8956ecc0e
Søren Furbo <srfu@novonordisk.com>
2024-04-30 13:11:08 +0200

    fix: Testing local import

--- applications/__init__.py
+++ applications/__init__.py
@@ -1,20 +1,20 @@
-from applications.empower_implementation.empower_tools import (
+from .empower_implementation.empower_tools import (
     determine_if_isocratic_method,
     determine_last_high_flow_time,
     determine_max_compositon_value,
     determine_strong_eluent,
     post_instrument_methodset_method,
 )
-from applications.method_generators.add_isocratic_segment import (
+from .method_generators.add_isocratic_segment import (
     generate_add_isocratic_segment_to_method,
 )
-from applications.method_generators.alter_strong_eluent_pct import (
+from .method_generators.alter_strong_eluent_pct import (
     generate_altered_strong_eluent_method_pct,
 )
-from applications.method_generators.alter_temperature import (
+from .method_generators.alter_temperature import (
     generate_altered_temperature_method,
 )
-from applications.method_generators.ramp_method import generate_ramp_method
+from .method_generators.ramp_method import generate_ramp_method
 
 __all__ = [
     "post_instrument_methodset_method",
