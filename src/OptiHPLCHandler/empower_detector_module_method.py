d52002d95855d42f7a5c2cb0af6b1c4332867b9d
SLCB-NN <slcb@novonordisk.com>
2024-07-10 14:18:47 +0200

    feat: spectra getter and setter pda

--- src/OptiHPLCHandler/empower_detector_module_method.py
+++ src/OptiHPLCHandler/empower_detector_module_method.py
@@ -29,7 +29,7 @@ def to_string(bool_value: Union[str, bool]) -> str:
 class Detector(EmpowerModuleMethod):
     def simplified_channel_name(self, channel_name: str) -> str:
         # Convert "ChannelA" to "Channel1" and "ChannelB" to "Channel2"
-        channel_number = ord(channel_name[-1])-64
+        channel_number = ord(channel_name[-1]) - 64
         # ord converts a character into an integer, so ord("A") = 65, ord("B") = 66, etc.
         if channel_number < 1:
             return channel_name
@@ -39,7 +39,7 @@ class Detector(EmpowerModuleMethod):
         if ord(channel_name[-1]) < 65:
             channel_number = int(channel_name[-1])
         else:
-            channel_number = ord(channel_name[-1])-64
+            channel_number = ord(channel_name[-1]) - 64
         try:
             self["ChannelA"]
             # If there is a ChannelA, the channels are designated alphabetically
@@ -61,12 +61,14 @@ class TUVMethod(Detector):
     wavelength_name = "Wavelength"
 
     @property
-    def lamp_enabled(self) -> bool:
-        return self["Lamp"] == "true"
-
-    @lamp_enabled.setter
-    def lamp_enabled(self, value: bool):
-        self["Lamp"] = to_string(value)
+    def wavelength_mode(self) -> str:
+        # Not settable as would have to change "DataMode" which is hell
+        if self["WavelengthMode"] == "1":
+            return "Single"
+        elif self["WavelengthMode"] == "2":
+            return "Dual"
+        else:
+            raise ValueError(f"Invalid WavelengthMode: {self['WavelengthMode']}")
 
     @property
     def channel_dict(self) -> dict[str, dict]:
@@ -76,12 +78,11 @@ class TUVMethod(Detector):
             channel_xml = f"<{channel_name}>{self[channel_name]}</{channel_name}>"
             channel = ET.fromstring(channel_xml)
             wavelength = channel.find(self.wavelength_name).text
-            data_mode = channel.find("DataMode").text
             channel_dict[channel_name] = {
                 self.wavelength_name: wavelength,
-                "DataMode": data_mode,
+                "Type": "Single",
                 "XML": channel_xml,
-            }  # enabled True is a bit of a lie, but this brings consistency with the PDA method
+            }
         return channel_dict
 
     @channel_dict.setter
@@ -96,8 +97,6 @@ class TUVMethod(Detector):
             channel_str = channel_str.replace(f"<{channel_name}> ", "")
             channel_str = channel_str.replace(f" </{channel_name}>", "")
             self[channel_name] = channel_str
-
-    # setting channel_dict by {"ChannelA": {"Wavelength": 666}}
 
 
 class PDAMethod(Detector):
@@ -114,16 +113,28 @@ class PDAMethod(Detector):
             channel = ET.fromstring(channel_xml)
             wavelength = channel.find(self.wavelength_name).text
             enabled = to_bool(channel.find("Enable").text)
-            data_mode = channel.find("DataMode").text
             channel_dict[channel_name] = {
                 self.wavelength_name: wavelength,
-                "Enabled": enabled,
-                "DataMode": data_mode,
+                "Enable": enabled,
+                "Type": "Single",
                 "XML": channel_xml,
             }
 
-        # Spectral channels
-        # deal with that mess here
+        spectral_channel_xml = (
+            f"<SpectralChannel>{self['SpectralChannel']}</SpectralChannel>"
+        )
+        spectral_channel = ET.fromstring(spectral_channel_xml)
+        start_wavelength = spectral_channel.find("StartWavelength").text
+        end_wavelength = spectral_channel.find("EndWavelength").text
+        enabled = to_bool(spectral_channel.find("Enable").text)
+        channel_dict["SpectralChannel"] = {
+            "StartWavelength": start_wavelength,
+            "EndWavelength": end_wavelength,
+            "Enable": enabled,
+            "Type": "Spectral",
+            "XML": spectral_channel_xml,
+        }
+
         return channel_dict
 
     @channel_dict.setter
@@ -131,20 +142,37 @@ class PDAMethod(Detector):
         for channel_name, channel in value.items():
             old_xml = f"<{channel_name}>{self[channel_name]}</{channel_name}>"
             old_channel = ET.fromstring(old_xml)
-            for setting_name, setting_value in channel.items():
-                if setting_name != "XML":
-                    old_channel.find(setting_name).text = str(setting_value)
-            channel_str = ET.tostring(old_channel).decode()
-            channel_str = channel_str.replace(f"<{channel_name}> ", "")
-            channel_str = channel_str.replace(f" </{channel_name}>", "")
-            self[channel_name] = channel_str
+            if channel_name.startswith("Channel"):
+                for setting_name, setting_value in channel.items():
+                    if setting_name != "XML":
+                        if setting_name == "Enable":
+                            setting_value = to_string(setting_value)
+                        old_channel.find(setting_name).text = str(setting_value)
+                channel_str = ET.tostring(old_channel).decode()
+                channel_str = channel_str.replace(f"<{channel_name}> ", "")
+                channel_str = channel_str.replace(f" </{channel_name}>", "")
+                self[channel_name] = channel_str
+            elif channel_name == "SpectralChannel":
+                for setting_name, setting_value in channel.items():
+                    if setting_name != "XML":
+                        if setting_name == "Enable":
+                            setting_value = to_string(setting_value)
+                        old_channel.find(setting_name).text = str(setting_value)
+                channel_str = ET.tostring(old_channel).decode()
+                channel_str = channel_str.replace(f"<{channel_name}> ", "")
+                channel_str = channel_str.replace(f" </{channel_name}>", "")
+                self[channel_name] = channel_str
+            else:
+                raise ValueError(f"Invalid channel type: {old_channel.get('Type')}")
 
 
 class FLRMethod(Detector):
     @property
     def channel_dict(self) -> list[dict]:
         channel_dict = {}
-        channel_name_list = [self.empower_channel_name(f"Channel{num}") for num in range(1, 5)]
+        channel_name_list = [
+            self.empower_channel_name(f"Channel{num}") for num in range(1, 5)
+        ]
         for channel_name in channel_name_list:
             try:
                 channel_xml = f"<{channel_name}>{self[channel_name]}</{channel_name}>"
