6933e66fa0955055a54be233d5064fca19ba6a23
jxow <jxow@novonordisk.com>
2024-09-18 08:47:18 +0000

    added empower_session_method.py allowing logging out all user sessions

--- src/OptiHPLCHandler/__init__.py
+++ src/OptiHPLCHandler/__init__.py
@@ -2,6 +2,7 @@ from .empower_api_core import EmpowerConnection
 from .empower_handler import EmpowerHandler
 from .empower_instrument_method import EmpowerInstrumentMethod
 from .empower_module_method import EmpowerModuleMethod
+from .empower_session_method import EmpowerSession
 
 __version__ = "3.5.0"
 
@@ -10,4 +11,5 @@ __all__ = [
     "EmpowerHandler",
     "EmpowerInstrumentMethod",
     "EmpowerModuleMethod",
+    "EmpowerSession",
 ]
