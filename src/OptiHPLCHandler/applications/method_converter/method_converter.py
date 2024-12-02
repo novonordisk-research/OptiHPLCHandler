d2bc9b7d528047c710722f24bc48c819e350eb09
SLCB-NN <slcb@novonordisk.com>
2024-12-02 11:51:35 +0100

    feat: code review

--- src/OptiHPLCHandler/applications/method_converter/method_converter.py
+++ src/OptiHPLCHandler/applications/method_converter/method_converter.py
@@ -7,7 +7,7 @@ from OptiHPLCHandler.applications.empower_implementation.empower_tools import (
 logger = logging.getLogger(__name__)
 
 
-def change_gradient_table(  # noqa C901 # NEEDS REVIEW! SRFU
+def change_gradient_table(
     input_gradient_table: list[dict],
     output_gradient_table: list[dict],
 ) -> dict:
@@ -43,23 +43,27 @@ def change_gradient_table(  # noqa C901 # NEEDS REVIEW! SRFU
     """
     logger.debug(f"Input gradient table: {input_gradient_table}")
     logger.debug(f"Initial output gradient table: {output_gradient_table}")
+    input_lines = [
+        key for key in input_gradient_table[0].keys() if "Composition" in key
+    ]
+    output_lines = [
+        key for key in output_gradient_table[0].keys() if "Composition" in key
+    ]
+    if set(input_lines) == set(output_lines):
+        logger.debug(
+            "Like to like method transfer, returning gradient table unchanged."
+        )
+        return input_gradient_table
 
     # Determine eluent strength
     classification_input = classify_eluents(input_gradient_table)
     classification_output = classify_eluents(output_gradient_table)
 
-    # Determine that number of compositions in the output method
-    output_lines = [
-        key for key in output_gradient_table[0].keys() if "Composition" in key
-    ]
-    input_lines = [
-        key for key in input_gradient_table[0].keys() if "Composition" in key
-    ]
-
     logger.debug(f"Classification of eluents in input method: {classification_input}")
     logger.debug(f"Classification of eluents in output method: {classification_output}")
 
     logger.debug("Determining eluent composition of input and output methods.")
+    new_gradient_table = []
     if (
         len(classification_input["strong_eluents"]) == 1
         and len(classification_input["weak_eluents"]) == 1
@@ -84,27 +88,17 @@ def change_gradient_table(  # noqa C901 # NEEDS REVIEW! SRFU
         # Ensure strong eluent is composition B and weak eluent is composition A
         # Doesn't check if already in correct format
         logger.debug("Transferring gradient table to output method.")
-        new_gradient_table = []
+
         for step in input_gradient_table:
-            new_step = step.copy()  # Copy to prevent overwiting unread composition
-            # Weak eluent composition is as was in output method
-            new_step[output_weak_eluent] = step[input_weak_eluent]
-            # Strong eluent composition is as was in output method
-            new_step[output_strong_composition] = step[input_strong_composition]
-            # Set unused solvent lines to 0
-            if (
-                float(new_step[output_weak_eluent])
-                + float(new_step[output_strong_composition])
-                != 100
-            ):
-                raise ValueError(
-                    "The sum of the strong and weak eluent in the input method does not equal 100. The gradient is either invalid or another composition has a non zero value which is not supported."  # noqa E501
-                )
+            new_step = {
+                output_weak_eluent: step[input_weak_eluent],
+                output_strong_composition: step[input_strong_composition],
+                "Time": step["Time"],
+                "Flow": step["Flow"],
+                "Curve": step["Curve"],
+            }
             for line in unused_output_solvent_lines:
                 new_step[line] = 0.0
-            new_step["Time"] = step["Time"]
-            new_step["Flow"] = step["Flow"]
-            new_step["Curve"] = step["Curve"]
             new_gradient_table.append(new_step)
 
     elif (
@@ -115,104 +109,30 @@ def change_gradient_table(  # noqa C901 # NEEDS REVIEW! SRFU
         logger.debug("Method is isocratic.")
 
         # Determine all the composition names in the input method
-        non_zero_compositions = set(
+        non_zero_compositions = [
             composition
-            for step in input_gradient_table
-            for composition in step
+            for composition in input_gradient_table[0].keys()
             if composition not in ["Time", "Flow", "Curve"]
-            and float(step[composition]) != 0.0
-        )
+            and float(input_gradient_table[0][composition]) != 0.0
+        ]
         logger.debug(f"Non-zero compositions in input method: {non_zero_compositions}")
-
-        # Determine all the composition names in the output method
-        unused_output_solvent_lines = [
-            key for key in output_gradient_table[0].keys() if "Composition" in key
-        ]
-
-        # Remove compositions that have non-zero values in the input method
-        unused_output_solvent_lines = [
-            line
-            for line in unused_output_solvent_lines
-            if line not in non_zero_compositions
-        ]
-        logger.debug(
-            f"Unused solvent lines in output method: {unused_output_solvent_lines}"
-        )
-
-        # if bsm to bsm, bsm to qsm, qsm to bsm (max two compositions) or qsm to qsm
-        # else qsm to bsm (more than two compositions) not possible
-        if (
-            len(non_zero_compositions) <= 2
-        ):  # bsm to bsm, bsm to qsm, qsm to bsm (max two compositions)
-            logger.debug(
-                f"Number of compositions used in input method is {len(non_zero_compositions)} which is less than or equal to 2. Therefore the transfer is either bsm to bsm, bsm to qsm or qsm to bsm (max two combinations)"  # noqa E501
+        if len(non_zero_compositions) > len(output_lines):
+            raise ValueError(
+                "Method uses more compositions than output method. Cannot transfer gradient table."  # noqa E501
             )
-
-            logger.debug("Copying over gradient table to output method format.")
-            new_gradient_table = []
-            for step in input_gradient_table:
-                new_step = step.copy()
-            for line in unused_output_solvent_lines:
-                new_step[line] = "0.0"
+        for step in input_gradient_table:
+            unused_output_solvent_lines = output_lines.copy()
+            new_step = {line: "0.0" for line in output_lines}
+            for composition in non_zero_compositions:
+                new_step[unused_output_solvent_lines.pop(0)] = step[composition]
             new_step["Time"] = step["Time"]
             new_step["Flow"] = step["Flow"]
             new_step["Curve"] = step["Curve"]
             new_gradient_table.append(new_step)
-
-            if (
-                len(output_lines) == 2 and len(input_lines) == 4
-            ):  # QSM to BSM (max two compositions)
-                logger.debug("QSM to BSM (max two compositions).")
-
-                # if CompositionC or D used. They need to be swapped to A or B
-                if "CompositionC" in non_zero_compositions:
-                    unused_solvent_line = next(
-                        line
-                        for line in unused_output_solvent_lines
-                        if line not in ["CompositionC", "CompositionD"]
-                    )
-                    # Solvent line now used
-                    unused_output_solvent_lines.remove(unused_solvent_line)
-                    unused_output_solvent_lines.append("CompositionC")
-                    logger.debug(
-                        "CompositionC is used, when target is BSM, swapping..."
-                    )
-                    logger.debug(f"Swapping CompositionC to {unused_solvent_line}")
-                    for step in new_gradient_table:
-                        # Find the unused composition
-                        new_step[unused_solvent_line] = step.pop("CompositionC")
-                if "CompositionD" in non_zero_compositions:
-                    unused_solvent_line = next(
-                        line
-                        for line in unused_output_solvent_lines
-                        if line not in ["CompositionC", "CompositionD"]
-                    )
-                    # Solvent line now used
-                    unused_output_solvent_lines.remove(unused_solvent_line)
-                    unused_output_solvent_lines.append("CompositionD")
-                    logger.debug("CompositionD is used, swapping to CompositionB.")
-                    for step in new_gradient_table:
-                        step[unused_solvent_line] = step.pop("CompositionD")
-
-        else:  # more than two compositions
-            if len(output_lines) == 2:  # QSM to BSM (more than two compositions)
-                raise ValueError(
-                    "Method has more than two changing compositions and output method is BSM."  # noqa E501
-                )
-            else:  # QSM to QSM (more than two compositions)
-                logger.debug("QSM to QSM (more than two compositions).")
-                new_gradient_table = input_gradient_table
     else:
         # Too complicated.
         raise ValueError(
             "Method transfer of gradient tables with multiple strong or weak eluents not supported."  # noqa E501
         )
-
-    # Remove CompositionC and CompositionD if output method is BSM
-    if len(output_lines) == 2:  # BSM
-        logger.debug("Output method is BSM, removing CompositionC and CompositionD.")
-        for step in new_gradient_table:
-            step.pop("CompositionC", None)
-            step.pop("CompositionD", None)
 
     return new_gradient_table
