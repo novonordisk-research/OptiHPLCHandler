eb1efde5f2e4ef09ad7e70a21ccd94b60b46eda2
SLCB-NN <slcb@novonordisk.com>
2025-05-22 14:40:19 +0200

    fix: be more expicit in ordering of gradient table

--- src/OptiHPLCHandler/plotting/gradient_plot.py
+++ src/OptiHPLCHandler/plotting/gradient_plot.py
@@ -21,15 +21,27 @@ def order_gradient_table(gradient_table: list[dict]) -> list[dict]:
     """
     Orders the gradient table by Time.
     """
-    return sorted(
-        gradient_table, key=lambda entry: (entry["Time"] != "Initial", entry["Time"])
-    )
+    initial_row = None
+    other_rows = []
+    for row in gradient_table:
+        if row["Time"] == "Initial":
+            initial_row = row # Save to add to start later
+        else:
+            other_rows.append(row)
+    for row in other_rows:
+        row["Time"] = float(row["Time"]) # ensure float
+    other_rows.sort(key=lambda entry: entry["Time"]) # order list by
+    result = []
+    if initial_row is not None:
+        result.append(initial_row) # Adds initial to start of results list
+    result.extend(other_rows) # Adds rest of the rows after
+    return result
 
 
 def standardise_gradient_table_types(gradient_table: list[dict]) -> list[dict]:
     """
     Standardises the types in the gradient table.
-    Converts all values to floats, except for Time and Curve, which are converted to int
+    Converts all values to floats, except for Curve, which is converted to int
     """
 
     gradient_table = order_gradient_table(gradient_table)
